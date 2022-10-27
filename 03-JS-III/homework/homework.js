aedb13051cdec058c69f09e7a11d21401edd1749
alfredo Escalona <alfredo_escalona@yahoo.es>
2022-10-26 23:59:15 -0500

    1er. commit. Ejer. 01, 02 y 03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,56 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acum_ = 0;
+    for (var i = 1; i <= 10; i++) acum_= acum_ + i;
+    return acum_;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var array_ = [];
+
+  for(numero_ of array){
+    if (numero_%2===0){
+      array_.push(numero_);
+    }
+  }
+ return array_;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+
+  var array_ = [];
+  for(numero_ of array){
+    array_.push(numero_**2);
+  }
+return array_;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+
+  var num_ = 0;
+  for(numero_ of array){
+    num_ = num_ + numero_;
+  }
+return num_;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+
+  //String numCadena= Integer. toString(numEntero)
+  var cadena_ = num.toString();
+  return cadena_.length;
 }
   
   
